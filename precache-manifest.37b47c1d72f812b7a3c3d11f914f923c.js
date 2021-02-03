39cfa5862fe8202fc6d74b37ef537cd6efe1e9e2
kidchaos04 <vinayakrastogi94@gmail.com>
2021-02-03 23:24:16 +0530

    Updates

---
+++ precache-manifest.37b47c1d72f812b7a3c3d11f914f923c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7f91fd00fae038a330684b8d692463b",
+    "url": "/foodrecipe/index.html"
+  },
+  {
+    "revision": "b3cddbf896952b2d91f9",
+    "url": "/foodrecipe/static/css/main.2a72effa.chunk.css"
+  },
+  {
+    "revision": "7e93996d0d3e5a978794",
+    "url": "/foodrecipe/static/js/2.b966dc89.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/foodrecipe/static/js/2.b966dc89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3cddbf896952b2d91f9",
+    "url": "/foodrecipe/static/js/main.119cb7e6.chunk.js"
+  },
+  {
+    "revision": "7b5d168918be472feab3",
+    "url": "/foodrecipe/static/js/runtime-main.e19a0a69.js"
+  }
+]);
